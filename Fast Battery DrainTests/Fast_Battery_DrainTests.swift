a3b6f2239ae91a515798b1af74c6aa246241df84
muhammadumairali093456 <82758044+muhammadumairali093456@users.noreply.github.com>
2021-04-18 18:20:20 +0000

    Add files via upload
    Third Commit

---
+++ Fast Battery DrainTests/Fast_Battery_DrainTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fast_Battery_DrainTests.swift
+//  Fast Battery DrainTests
+//
+//  Created by Muhammad umair ali on 21/01/2019.
+//  Copyright © 2019 Muhammad umair ali. All rights reserved.
+//
+
+import XCTest
+@testable import Fast_Battery_Drain
+
+class Fast_Battery_DrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
